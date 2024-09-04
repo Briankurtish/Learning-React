7e94cfd150d7394d69f116ec689531f9b67da703
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-09-04 14:00:26 +0100

    using props in react

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+// { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
   //   Hook -- it is a function that allows us to tap into built in features in react
   const [selectedIndex, setSelectedIndex] = useState(-1);
-
-  //   items = [];
-  //   Event Handler
-  //   const handleClick = (event: MouseEvent) => console.log(event);
 
   const getMessage = () => {
     return items.length === 0 ? <p>No Items found</p> : null;
@@ -16,7 +16,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* {items.length === 0 ? <p>No Items found</p> : null} */}
       {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
