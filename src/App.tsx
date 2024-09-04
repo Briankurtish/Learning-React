a0bf205d1ace21475893a583aae8ac8714b766b6
Annon_101 <67450280+Briankurtish@users.noreply.github.com>
2024-09-04 14:07:32 +0100

    using props to pass functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
 
+  const handleSelectedItem = (item: string) => {
+    alert(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectedItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //   Hook -- it is a function that allows us to tap into built in features in react
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -25,6 +26,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
             className={
               selectedIndex === index
